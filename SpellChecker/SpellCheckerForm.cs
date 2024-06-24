9c1b415e1f6fa9d4899aaaa84db114f302803417
Elina <elina>
2024-06-24 22:31:20 +0400

    Add SpellingCorrection class with dictionary logic

--- SpellChecker/SpellCheckerForm.cs
+++ SpellChecker/SpellCheckerForm.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace SpellChecker
@@ -19,7 +12,37 @@ namespace SpellChecker
 
         private void buttonCheck_Click(object sender, EventArgs e)
         {
-            //TODO Добавить обработку входных данных
+            textBoxOutput.Clear();
+            string text = textBoxInput.Text + "\r\n";
+            string[] input = text.Split(new string[] { "===\r" }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                if (input.Length <= 2)
+                    throw new IndexOutOfRangeException();
+
+                string dictionary = input[0];
+                SpellingCorrection spelling = new SpellingCorrection(dictionary);
+
+                string[] linesToCorrection = input[1].Split(new string[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                string[] correctedTextLines = spelling.CorrectTextLines(linesToCorrection);
+
+                foreach (string line in correctedTextLines)
+                {
+                    textBoxOutput.Text += line;
+                    textBoxOutput.Text += Environment.NewLine;
+                }
+            }
+            catch (IndexOutOfRangeException)
+            {
+                MessageBox.Show("Invalid input", "Error");
+            }
+            catch (SpellingCorrection.DictionaryException ex)
+            {
+                MessageBox.Show(ex.Message, "Error");
+            }
+
         }
     }
 }
---
+++ SpellChecker/SpellingCorrection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpellChecker
+{
+    public class SpellingCorrection
+    {
+        public HashSet<string> DictionaryWords { get; }
+
+        public SpellingCorrection(string dictionaryText)
+        {
+            string[] dictionary = dictionaryText
+                .Split(new string[] { "\r\n", " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(word => word.Length <= 50)
+                .ToArray();
+            if (dictionary.Length == 0) throw new DictionaryException("Empty dictionary");
+            DictionaryWords = new HashSet<string>(dictionary, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public string[] CorrectTextLines(string[] textLines)
+        {
+            //TODO Добавить проверку
+            return textLines;
+        }
+
+        public class DictionaryException : Exception
+        {
+            public DictionaryException(string message) : base(message) { }
+        }
+    }
+}
